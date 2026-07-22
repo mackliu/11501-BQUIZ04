1fa5b878e5e134a1fc5fe01daeed58f3090c8879
mackliu <macklun@gmail.com>
2026-07-22 13:54:42 +0800

    建立商品清單

--- back/th.php
+++ back/th.php
@@ -83,18 +83,23 @@ function getBigs(){
         <td>商品名稱</td>
         <td>庫存量</td>
         <td>狀態</td>
-        <td>操作</td>
+        <td style="width:150px;">操作</td>
     </tr>
+    <?php
+    $items=$Item->all();
+    foreach($items as $item):
+    ?>
     <tr class="pp ct">
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
+        <td><?= $item['no'] ?></td>
+        <td><?= $item['name'] ?></td>
+        <td><?= $item['qt'] ?></td>
+        <td><?= ($item['sh']==1)?'販售中':'已下架'; ?></td>
         <td>
-            <button>修改</button>
-            <button>刪除</button>
-            <button>上架</button>
-            <button>下架</button>
+            <button onclick="location.href='?do=edit_item&id=<?= $item['id'] ?>'">修改</button>
+            <button onclick="del('Item',<?= $item['id']; ?>)">刪除</button>
+            <button onclick="sh(1,<?= $item['id']; ?>)">上架</button>
+            <button onclick="sh(2,<?= $item['id']; ?>)">下架</button>
         </td>
     </tr>
+    <?php endforeach ;?>
 </table>
